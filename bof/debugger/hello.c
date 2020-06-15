2381317cc9a1c710083d91b7b9388cf61736fcac
cyberblackhole <willyoumailme7@gmail.com>
2020-06-15 16:03:18 +0530

    sed

---
+++ bof/debugger/hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void flag(){
+	printf("Here is your flag");
+}
+
+
+int main(){
+	int value=100;
+	char name[25]="cyber blackhole";
+	printf("Name is %s \n",name);
+	printf("Value is %d \n",value);
+	return 0;
+}
